2f205060f818527ccfc48c5e1c8cf88eb7bc9e86
Takuya Sawada <takuya@tuntunkun.com>
2017-10-12 19:50:47 +0900

    Revert "add TODO comment"
    This reverts commit c5a2d05209ded0b038ff1e06199c2d8e4f9a60df.

--- src/shoghicp/BigBrother/network/protocol/Play/Server/SpawnObjectPacket.php
+++ src/shoghicp/BigBrother/network/protocol/Play/Server/SpawnObjectPacket.php
@@ -72,7 +72,7 @@ class SpawnObjectPacket extends OutboundPacket{
 		$this->putAngle($this->pitch);
 		$this->putAngle($this->yaw);
 		$this->putInt($this->data);
-		if($this->data > 0){//TODO make sure whther if this condition is really needed
+		if($this->data > 0){
 			$this->putShort((int) round($this->velocityX * 8000));
 			$this->putShort((int) round($this->velocityY * 8000));
 			$this->putShort((int) round($this->velocityZ * 8000));
